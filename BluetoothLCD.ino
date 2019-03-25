3dc07448506f5c59bdf541a9407a57b9b4ff26e9
Vincent Kok <vick.kok@live.com>
2019-03-25 12:59:09 +0800

    Added Arduino code

---
+++ BluetoothLCD.ino
@@ -0,0 +1,32 @@
+#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+LiquidCrystal lcd(12, 11, 9, 8, 7, 6);
+char buffer[16];
+int charsRead;
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);       
+    lcd.begin(16, 2);
+    lcd.print("Welcome!");
+    delay(3000);
+    lcd.clear();
+}
+void loop() {
+ 
+if(mySerial.available() > 0){
+
+charsRead = mySerial.readBytesUntil('\n', buffer, sizeof(buffer)-1);
+lcd.clear();
+buffer[charsRead] = '\0';
+
+lcd.print(buffer);
+
+delay(4000);
+
+}
+}
+
+
